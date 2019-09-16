8e041cc7458a9eadc2340b8bdb396d5b8bbd7ea1
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-16 02:20:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,7 +2,6 @@ class EmailParser
   attr_accessor :name, :csv_emails
 
   def initialize(csv_emails)
-
     @csv_emails = csv_emails
   end
 
@@ -13,3 +12,10 @@ class EmailParser
     .flatten.uniq
   end
 end
+ emails1 = "asd2GW.com,per@er.org, Zaza.net"
+ parser1 = EmailParser.new(emails1)
+ parser1.parse
+
+ emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+ parser2 = EmailParser.new(emails2)
+ parser2.parse 
