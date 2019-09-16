69519a605c73a8e3e0be12d96326c54a661a4c01
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-16 05:18:51 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,8 +3,13 @@ attr_accessor :name, :csv_emails
 
 
 def initialize(csv_emails)
+@name = name 
 @csv_emails = csv_emails
 end
+
+def parse 
+  csv_emails.split(/\s*,\s*/)
+end 
 
 def parse
     puts csv_emails.split.collect do |address| address.split(',')
