5beca6588b18eb35f33f12b8b057e5aa99878266
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-16 04:53:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,10 +13,10 @@ def parse
   end
 end
 
-emails1 = "asd2GW.com,per@er.org, Zaza.net"
+emails1 = "avi@test.com,arel@test.com, test@avi.com"
 parser1 = EmailParser.new(emails1)
 parser1.parse
 
-emails2 = "asd@qw.com per@er.org, per@er.org, Zaza@sugar.net"
+emails2 = "avi@test.com, avi@test.com"
 parser2 = EmailParser.new(emails2)
 parser2.parse
